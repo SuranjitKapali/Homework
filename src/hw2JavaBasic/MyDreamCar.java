29614fe46b3f85b1f8cfbc5e882a92958b95aa65
SuranjitKapali <suranjitkapali@gmail.com>
2024-01-04 18:33:55 -0800

    Update

--- src/hw2JavaBasic/MyDreamCar.java
+++ src/hw2JavaBasic/MyDreamCar.java
@@ -7,7 +7,8 @@ public class MyDreamCar {
 		System.out.println("\tMy name is Suranjit Kapali," + " and I wish to drive Lamborghini.");
 		System.out.println("\n\tMy car info is below:");
 		System.out.print(
-				"\n\tColor: Nero Noctis, \n\tModel: Revuelto, \n\tYear: 2023, \n\tWheel Drive: 4WD, \n\tSpeed: 217 mph, \n\tCurb Weight: 3906lb\n\tPrice: $604,363.");
+				"\n\tColor: Nero Noctis, \n\tModel: Revuelto, \n\tYear: 2023, \n\tWheel Drive: 4WD, "
+				+ "\n\tSpeed: 217 mph, \n\tCurb Weight:"+ " 3906lb\n\tPrice: $604,363.");
 	}
 
 }
